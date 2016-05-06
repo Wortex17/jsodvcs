39bdeea738f145e13106eacd2e5c955fa001c4cf
Patrick Michael Hopf <dragonia@gmx.at>
2016-05-06 11:23:51 +0200

    Use regex to identify ref names

--- spec/Repository/ref.js
+++ spec/Repository/ref.js
@@ -11,6 +11,44 @@ let
 ;
 
 exports.spec = function(){
+
+
+    describe.only('Repository.isRef(ref)', function() {
+
+        context("when valid ref syntax", function() {
+            it("should return true", function () {
+                expect(jsodvcs.Repository.isRef("refs/heads/master")).to.be.true;
+                expect(jsodvcs.Repository.isRef("refs/remotes/origin/master")).to.be.true;
+                expect(jsodvcs.Repository.isRef("refs/heads/slash/inbetween")).to.be.true;
+                expect(jsodvcs.Repository.isRef("refs/remotes/origin/slash/inbetween")).to.be.true;
+                expect(jsodvcs.Repository.isRef("refs/heads/num0inbetween")).to.be.true;
+                expect(jsodvcs.Repository.isRef("refs/remotes/origin/num0inbetween")).to.be.true;
+                expect(jsodvcs.Repository.isRef("HEAD")).to.be.true;
+                expect(jsodvcs.Repository.isRef("FETCH_HEAD")).to.be.true;
+                expect(jsodvcs.Repository.isRef("MERGE_HEAD")).to.be.true;
+            });
+        });
+        context("when no parameter given", function() {
+            it("should return false", function () {
+                expect(jsodvcs.Repository.isRef()).to.be.false;
+            });
+        });
+        context("when invalid ref syntax", function() {
+            it("should return false", function () {
+                expect(jsodvcs.Repository.isRef("heads/master")).to.be.false;
+                expect(jsodvcs.Repository.isRef("refs/master")).to.be.false;
+                expect(jsodvcs.Repository.isRef("refs/headss/master")).to.be.false;
+                expect(jsodvcs.Repository.isRef("refs/remotes/master")).to.be.false;
+                expect(jsodvcs.Repository.isRef("refs/origin/master")).to.be.false;
+                expect(jsodvcs.Repository.isRef("FOOBAR")).to.be.false;
+                expect(jsodvcs.Repository.isRef("refs/heads//slashstart")).to.be.false;
+                expect(jsodvcs.Repository.isRef("refs/heads/slashend/")).to.be.false;
+                expect(jsodvcs.Repository.isRef("refs/heads/0numstart")).to.be.false;
+                expect(jsodvcs.Repository.isRef("refs/heads/numend0")).to.be.false;
+            });
+        });
+    });
+
     describe('Repository#get_ref(ref)', function() {
 
         context("when getting missing ref 'refs/heads/master'", function() {
@@ -73,13 +111,13 @@ exports.spec = function(){
             });
         });
 
-        context("when setting existing ref 'refs/this/is/really/really/deep' to stored objectHash", function() {
+        context("when setting existing ref 'refs/heads/this/is/really/really/deep' to stored objectHash", function() {
             let repo = new jsodvcs.Repository();
             repo.objects['@re'] = {};
             repo.objects['@re']['fpointer@hash@'] = "foobar";
             repo.objects['@re']['fpointer@hash@2'] = "foobar2";
 
-            repo.set_ref("refs/this/is/really/really/deep", "@refpointer@hash@");
+            repo.set_ref("refs/heads/this/is/really/really/deep", "@refpointer@hash@");
 
             it("should return the repository", function () {
                 expect(repo.set_ref("refs/this/is/really/really/deep", "@refpointer@hash@2")).to.be.equal(repo);
