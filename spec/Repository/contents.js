508855d1949fc9bbc518345d100fc89f98f5f242
Patrick Michael Hopf <dragonia@gmx.at>
2016-05-09 21:55:40 +0200

    Implement get_ for blob, commit and tree objects

--- spec/Repository/contents.js
+++ spec/Repository/contents.js
@@ -79,6 +79,209 @@ exports.spec = function(){
         });
     });
 
+    describe('Repository#get_blob_object(path)', function() {
+
+        context("when trying to get missing object", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let hash = repo.add("foo/bar", content).get_indexed_hash("foo/bar");
+            let ret = repo.get_blob_object("dfndlfsd32f2fef2f");
+
+            it('should return undefined', function () {
+                expect(ret).to.be.undefined;
+            });
+        });
+        context("when trying to get blob object", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let hash = repo.add("foo/bar", content).get_indexed_hash("foo/bar");
+            let ret = repo.get_blob_object(hash);
+
+            it('should return the stored object', function () {
+                expect(ret).to.equal(content);
+            });
+        });
+        context("when trying to get commit object", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let out = {commitHash: "", commit: undefined};
+            repo.add("foo/bar", content).commit("", {out:out});
+            let ret = repo.get_blob_object(out.commitHash);
+
+            it('should return undefined', function () {
+                expect(ret).to.be.undefined;
+            });
+        });
+        context("when trying to get tree object", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let out = {commitHash: "", commit: undefined};
+            repo.add("foo/bar", content).commit("", {out:out});
+            let ret = repo.get_blob_object(out.commit.tree);
+
+            it('should return undefined', function () {
+                expect(ret).to.be.undefined;
+            });
+        });
+    });
+    describe('Repository#get_commit_object(path)', function() {
+
+        context("when trying to get missing object", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let hash = repo.add("foo/bar", content).get_indexed_hash("foo/bar");
+            let ret = repo.get_commit_object("dfndlfsd32f2fef2f");
+
+            it('should return undefined', function () {
+                expect(ret).to.be.undefined;
+            });
+        });
+        context("when trying to get blob object via hash", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let hash = repo.add("foo/bar", content).get_indexed_hash("foo/bar");
+            let ret = repo.get_commit_object(hash);
+
+            it('should return undefined', function () {
+                expect(ret).to.be.undefined;
+            });
+        });
+        context("when trying to get commit object via hash", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let out = {commitHash: "", commit: undefined};
+            repo.add("foo/bar", content).commit("", {out:out});
+            let ret = repo.get_commit_object(out.commitHash);
+
+            it('should return the created commit object', function () {
+                expect(ret).to.equal(out.commit);
+            });
+        });
+        context("when trying to get tree object via hash", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let out = {commitHash: "", commit: undefined};
+            repo.add("foo/bar", content).commit("", {out:out});
+            let ret = repo.get_commit_object(out.commit.tree);
+
+            it('should return undefined', function () {
+                expect(ret).to.be.undefined;
+            });
+        });
+    });
+    describe('Repository#get_commit_object(path)', function() {
+
+        context("when trying to get missing object", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let hash = repo.add("foo/bar", content).get_indexed_hash("foo/bar");
+            let ret = repo.get_commit_object("dfndlfsd32f2fef2f");
+
+            it('should return undefined', function () {
+                expect(ret).to.be.undefined;
+            });
+        });
+        context("when trying to get blob object via hash", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let hash = repo.add("foo/bar", content).get_indexed_hash("foo/bar");
+            let ret = repo.get_commit_object(hash);
+
+            it('should return undefined', function () {
+                expect(ret).to.be.undefined;
+            });
+        });
+        context("when trying to get commit object via hash", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let out = {commitHash: "", commit: undefined};
+            repo.add("foo/bar", content).commit("", {out:out});
+            let ret = repo.get_commit_object(out.commitHash);
+
+            it('should return the created commit object', function () {
+                expect(ret).to.equal(out.commit);
+            });
+        });
+        context("when trying to get tree object via hash", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let out = {commitHash: "", commit: undefined};
+            repo.add("foo/bar", content).commit("", {out:out});
+            let ret = repo.get_commit_object(out.commit.tree);
+
+            it('should return undefined', function () {
+                expect(ret).to.be.undefined;
+            });
+        });
+        context("when trying to get tree object via ref HEAD", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let out = {commitHash: "", commit: undefined};
+            repo.add("foo/bar", content).commit("", {out:out});
+            let ret = repo.get_commit_object("HEAD");
+
+            it('should return the latest commit', function () {
+                expect(ret).to.equal(out.commit);
+            });
+        });
+    });
+
+    describe('Repository#get_tree_object(path)', function() {
+
+        context("when trying to get missing object", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let hash = repo.add("foo/bar", content).get_indexed_hash("foo/bar");
+            let ret = repo.get_tree_object("dfndlfsd32f2fef2f");
+
+            it('should return undefined', function () {
+                expect(ret).to.be.undefined;
+            });
+        });
+        context("when trying to get blob object via hash", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let hash = repo.add("foo/bar", content).get_indexed_hash("foo/bar");
+            let ret = repo.get_tree_object(hash);
+
+            it('should return undefined', function () {
+                expect(ret).to.be.undefined;
+            });
+        });
+        context("when trying to get commit object via hash", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let out = {commitHash: "", commit: undefined};
+            repo.add("foo/bar", content).commit("", {out:out});
+            let ret = repo.get_tree_object(out.commitHash);
+
+            it('should return the tree of the commit object', function () {
+                expect(ret).to.equal(repo.objects.get(out.commit.tree));
+            });
+        });
+        context("when trying to get tree object via hash", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let out = {commitHash: "", commit: undefined};
+            repo.add("foo/bar", content).commit("", {out:out});
+            let ret = repo.get_tree_object(out.commit.tree);
+
+            it('should return the tree', function () {
+                expect(ret).to.equal(repo.objects.get(out.commit.tree));
+            });
+        });
+        context("when trying to get tree object via ref HEAD", function() {
+            let repo = new jsodvcs.Repository();
+            let content = {n:42};
+            let out = {commitHash: "", commit: undefined};
+            repo.add("foo/bar", content).commit("", {out:out});
+            let ret = repo.get_tree_object("HEAD");
+
+            it('should return the tree of the referenced (latest) commit', function () {
+                expect(ret).to.equal(repo.objects.get(out.commit.tree));
+            });
+        });
+    });
 
     describe('Repository.objects (RepositoryObjectStorage)', function() {
 
@@ -99,13 +302,27 @@ exports.spec = function(){
                 repo.objects.storage['abc'] = {'defghijk': false};
                 expect(repo.objects.get('abcdefghijk')).to.be.undefined;
             });
-            it('should return undefined when obejcts was found but does not have correct flags', function () {
+            it('should return undefined when object was found but does not have correct flags', function () {
                 repo.objects.set("0123456789", "foobar",  {setFlags: ['xxx']});
                 expect(repo.objects.get('0123456789', {flags: []})).to.be.undefined;
             });
-            it('should return the object when objects was found and has the correct flags', function () {
+            it('should return the object when object was found and has the correct flags', function () {
                 repo.objects.set("0123456789", "foobar", {setFlags: ['xxx']});
                 expect(repo.objects.get('0123456789', {flags: ['xxx']})).to.equal("foobar");
+            });
+        });
+
+        describe('RepositoryObjectStorage#getFlags(objectHash)', function() {
+            let repo = new jsodvcs.Repository();
+            repo.add("foo/bar", "foobar");
+            it('should return undefined when storage has storaged content in wrong internal format', function () {
+                repo.objects.storage['abc'] = {'defghijk': false};
+                expect(repo.objects.getFlags('abcdefghijk')).to.be.undefined;
+            });
+            it('should return empty array when object was found and has no flags', function () {
+                repo.objects.set("0123456789", "foobar");
+                expect(repo.objects.getFlags('0123456789')).to.be.an.array;
+                expect(repo.objects.getFlags('0123456789')).to.have.length(0);
             });
         });
     });
